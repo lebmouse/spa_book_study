d2a401a6221db632be656345ac784ffde127eb57
lebmouse <gtah2yk@gmail.com>
2018-12-25 23:56:28 +0900

    픽스 : spa의 return 수정
    return이 initModule에 있어서
    spa가 모듈패턴을 수행 못함

--- chapter_two/spa/js/spa.js
+++ chapter_two/spa/js/spa.js
@@ -10,10 +10,9 @@ var spa = (function () {
   var initModule = function ($container) {
     $container.html(
       `<h1 style="display:inline-block; margin:25px;">
-      hellow world!
+      hello world!
       </h1>`
     );
-    
-    return { initModule: initModule};
   }
+  return { initModule: initModule};
 }());
